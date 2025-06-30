14441afe16240c0e98d54f8b868f71eea60dbab4
dagiikg9134 <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-06-30 22:43:22 +0300

    Create bw.ts

---
+++ src/data/bw.ts
@@ -0,0 +1,18 @@
+export default {
+  theme: {
+    "bg color": "#000000",
+    "fg color": "#ffffff",
+    "main accent": "#8C001A",
+    "accent 1": "#a7c080",
+    "accent 2": "#e67e80",
+    "accent 3": "#7fbbb3",
+    "accent 4": "#d699b6",
+    "accent 5": "#c6a0f6",
+    "panel opacity": 0.3,
+  },
+  image: {
+    image: `url(${import.meta.env.BASE_URL}akira-explosion.jpg)`,
+    "position x": "52%",
+    "position y": "50%",
+  },
+};
