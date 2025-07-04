7e4d332dcfc64d9e1c0f1292919f7cbc22584de7
dagiikg9134 <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-07-04 16:41:38 +0300

    Add files via upload

---
+++ src/data/themes/Obsidian-Purple.ts
@@ -0,0 +1,18 @@
+export default {
+    theme: {
+        "bg color": "#05010C",
+        "fg color": "#804CDC",
+        "main accent": "#DECCFF",
+        "accent 1": "#82A46B",
+        "accent 2": "#D04236",
+        "accent 3": "#17EAE0",
+        "accent 4": "#B950C5",
+        "accent 5": "#EAD900",
+        "panel opacity": 0.5,
+    },
+    image: {
+        image: `url(${import.meta.env.BASE_URL}purple.jpg)`,
+        "position x": "50%",
+        "position y": "50%",
+    },
+};
---
+++ src/data/themes/Oxo Carbon.ts
@@ -0,0 +1,18 @@
+export default {
+    theme: {
+        "bg color": "#161616",
+        "fg color": "#D64B87",
+        "main accent": "#FFFFFF",
+        "accent 1": "#FFE97B",
+        "accent 2": "#42BE65",
+        "accent 3": "#3DDBD9",
+        "accent 4": "#33B1FF",
+        "accent 5": "#FF7EB6",
+        "panel opacity": 0.5,
+    },
+    image: {
+        image: `url(${import.meta.env.BASE_URL}oxo.png)`,
+        "position x": "50%",
+        "position y": "50%",
+    },
+};
