1e1a80395036f8d1857cb47032f715eeba83d21a
dagiikg9134 <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-07-05 20:26:36 +0300

    Add files via upload

---
+++ src/data/themes/Catppuccin Latte.ts
@@ -0,0 +1,18 @@
+export default {
+    theme: {
+        "bg color": "#EFF1F5",
+        "fg color": "#4C4F69",
+        "main accent": "#EC92BF",
+        "accent 1": "#40A02B",
+        "accent 2": "#D20F39",
+        "accent 3": "#1E66F5",
+        "accent 4": "#EA76CB",
+        "accent 5": "#DF8E1D",
+        "panel opacity": 0.5,
+    },
+    image: {
+        image: `url(${import.meta.env.BASE_URL}latte.jpg)`,
+        "position x": "50%",
+        "position y": "50%",
+    },
+};
---
+++ src/data/themes/Rosé Pine.ts
@@ -0,0 +1,18 @@
+export default {
+    theme: {
+        "bg color": "#191724",
+        "fg color": "#EEBDC9",
+        "main accent": "#ABC8F4",
+        "accent 1": "#EBBCBA",
+        "accent 2": "#9CCFD8",
+        "accent 3": "#F6C177",
+        "accent 4": "#EB6F92",
+        "accent 5": "#C4A7E7",
+        "panel opacity": 0.5,
+    },
+    image: {
+        image: `url(${import.meta.env.BASE_URL}pink.png)`,
+        "position x": "50%",
+        "position y": "50%",
+    },
+};
